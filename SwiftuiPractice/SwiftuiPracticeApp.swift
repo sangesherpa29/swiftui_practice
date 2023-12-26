5db9817865745efa448bb595e8ab96764626648b
iSangeSherpa <sangesherpa1215@gmail.com>
2023-12-26 08:16:37 +0545

    Initial Commit

---
+++ SwiftuiPractice/SwiftuiPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiPracticeApp.swift
+//  SwiftuiPractice
+//
+//  Created by Web and App on 26/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
