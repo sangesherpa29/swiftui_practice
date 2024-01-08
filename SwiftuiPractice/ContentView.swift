fa355a2e5aa5e6496e0ed76871b2845d7f7ff2db
iSangeSherpa <sangesherpa1215@gmail.com>
2024-01-08 16:01:28 +0545

    minor changes

--- SwiftuiPractice/ContentView.swift
+++ SwiftuiPractice/ContentView.swift
@@ -9,26 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        FullProfileView()
-//        TabView {
-//            ChatTabView()
-//                .tabItem {
-//                    Image(systemName: "ellipsis.message")
-//                    Text("Chat")
-//                }
-//                .toolbarBackground(.visible, for: .tabBar)
-//                .toolbarBackground(Color.white, for: .tabBar)
-//            
-//            ProfileView()
-//                .tabItem {
-//                    Image(systemName: "person")
-//                    Text("Profile")
-//                }
-//                .toolbarBackground(.visible, for: .tabBar)
-//                .toolbarBackground(Color.white, for: .tabBar)
-//        }
-//        .frame(height: 850)
-//        .accentColor(.primaryColor)
+        TabView {
+            ChatTabView()
+                .tabItem {
+                    Image(systemName: "ellipsis.message")
+                    Text("Chat")
+                }
+                .toolbarBackground(.visible, for: .tabBar)
+                .toolbarBackground(Color.white, for: .tabBar)
+            
+            ProfileView()
+                .tabItem {
+                    Image(systemName: "person")
+                    Text("Profile")
+                }
+                .toolbarBackground(.visible, for: .tabBar)
+                .toolbarBackground(Color.white, for: .tabBar)
+        }
+        .frame(height: 850)
+        .accentColor(.primaryColor)
     }
 }
 
